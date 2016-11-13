aeef03d7518cfec76e47dd366fa2981c18dc8015
lisabertolini <lisabertolini06@gmail.com>
2016-11-13 22:15:04 +0100

    Update main.cpp

--- Es_34_pag70/main.cpp
+++ Es_34_pag70/main.cpp
@@ -4,6 +4,30 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int float n;
+    int m>=0;
+    int i, pot;
+    cout << "programma che premette di calcolare la potenza n alla m " << endl;
+    cout<<"inserire la base=n"<<endl;
+    cin>>x;
+    cout<<"inserire l'esponente=m"<<endl;
+    cin>>y;
+
+    if(m==0)
+       cout<<"il risultato di una potenza che ha come esponente 0 e' 1"<<endl;
+    if(m==1)
+        cout<<"il risultato di una potenza che ha come esponente 1 e'"<<x<<endl;
+        else
+        {
+            pot=n*n;
+        for(i=0;i=m;i++)
+        {
+            pot=pot*n;
+        }
+        }
+        if(n<0)
+        cout<<"il risultato e'"<<-pot<<endl;
+        if(n>0)
+        cout<<"il risultato e'"<<pot<<endl;
     return 0;
 }
