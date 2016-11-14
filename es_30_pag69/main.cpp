34c02569bb0239782afaf18ec9c42ecba4c6a3ce
lisabertolini <lisabertolini06@gmail.com>
2016-11-14 07:21:51 +0100

    Update main.cpp

--- es_30_pag69/main.cpp
+++ es_30_pag69/main.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int num, centinaia, decine, unita;
+    cout << "inserire un numero"<< endl;
+    cin>>num;
+    cout<<"il numero e' composto da":<<endl;
+    cout<<centinaia<<"centinaia"<<endl;
+    cout<<decinde<<"decine"<<endl;
+    cout<<unita<<"unita"<<endl;
+    do{
+    centinaia=num/100;
+    decine=(num-centinaia*100)/10;
+    unita=num-(centinaia*100+decine*10);
+    }while(num<=1000)
     return 0;
 }
