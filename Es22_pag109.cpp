0537c78c0f745f5bf9bf84f8df9e2fe31660752a
lisabertolini <00i_bertolini_lisa@brunoserver.local>
2017-02-20 09:46:21 +0100

    Es22_pag109

---
+++ Es22_pag109.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int invertire(int,int);
+int main(){
+int numero;
+int inverso=0;
+cout<<"inserisci un numero intero"<<endl;
+cin>>numero;
+cout<<"il numero inverso e'"<<invertire(numero,inverso)<<endl;
+}
+
+int invertire(int numero,int inverso)
+{
+   if(numero==0) return inverso;
+   else
+   {
+       inverso=inverso*10+numero%10;
+       return invertire(numero/10,inverso);
+
+}
+}
+
